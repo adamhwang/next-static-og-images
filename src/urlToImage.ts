0200a47b4e10e8824734cce916c6c0fa9d30a260
Adam Hwang <github@adamhwang.com>
2023-06-20 07:59:02 -0500

    Refactored urlToImage to ensure closing and limit parallelization

--- src/urlToImage.ts
+++ src/urlToImage.ts
@@ -1,50 +1,66 @@
 import * as fs from 'fs';
 import * as path from 'path';
-import { Viewport, ScreenshotOptions, PuppeteerLifeCycleEvent, launch, Page, ElementHandle} from 'puppeteer';
+import { map } from 'bluebird';
+import { Viewport, ScreenshotOptions, PuppeteerLifeCycleEvent, launch, Page, ElementHandle, Browser} from 'puppeteer';
 
 const urlToImage = async (options: Viewport & { type?: ScreenshotOptions["type"], ogImageSelector?: string, waitUntil: PuppeteerLifeCycleEvent }, urlPaths: { url: string, path: string }[]) => {
   const { type, ogImageSelector, waitUntil, ...viewPortOptions } = options;
 
-  const browser = await launch({ 
-    args: ['--no-sandbox', '--disable-setuid-sandbox'],
-    defaultViewport: viewPortOptions ? viewPortOptions : null,
-  });
-
-  await Promise.all(urlPaths.map(async (urlPath) => {
-    const dir = path.dirname(urlPath.path);
-
-    if (!fs.existsSync(dir)) {
-      fs.mkdirSync(dir, { recursive: true });
-    }
-
-    const page = await browser.newPage();
-
-    await page.goto(urlPath.url, {
-      waitUntil
+  const withBrowser = async (fn: (browser: Browser) => Promise<unknown>) => {
+    const browser = await launch({ 
+      args: ['--no-sandbox', '--disable-setuid-sandbox'],
+      defaultViewport: viewPortOptions ? viewPortOptions : null,
     });
+    try {
+      return await fn(browser);
+    } finally {
+      await browser.close();
+    }
+  }
   
-    let target: ElementHandle<Element> | null = null;
-    
-    if (ogImageSelector) {
-      try
-      {
-        await page.waitForSelector(ogImageSelector);
-        target = await page.$(ogImageSelector);
-      } catch {}
+  const withPage = (browser: Browser) => async (fn: (page: Page) => Promise<unknown>) => {
+    const page = await browser.newPage();
+    try {
+      return await fn(page);
+    } finally {
+      await page.close();
+    }
+  }
 
-      if (target) {
-        await target.screenshot({ omitBackground: true, path: urlPath.path, type });
+  await withBrowser(async (browser) => {
+    await map(urlPaths, async (urlPath) => {
+      const dir = path.dirname(urlPath.path);
+
+      if (!fs.existsSync(dir)) {
+        fs.mkdirSync(dir, { recursive: true });
       }
-    }
 
-    if (!target) {
-      await page.screenshot({ omitBackground: true, path: urlPath.path, type });
-    }
-    console.log(`Saved screenshot of ${urlPath.url} to ${urlPath.path}`);
-  
-    await page.close();
-  }));
-  await browser.close();
+      return withPage(browser)(async (page) => {
+        await page.goto(urlPath.url, {
+          waitUntil
+        });
+      
+        let target: ElementHandle<Element> | null = null;
+        
+        if (ogImageSelector) {
+          try
+          {
+            await page.waitForSelector(ogImageSelector);
+            target = await page.$(ogImageSelector);
+          } catch {}
+
+          if (target) {
+            await target.screenshot({ omitBackground: true, path: urlPath.path, type });
+          }
+        }
+
+        if (!target) {
+          await page.screenshot({ omitBackground: true, path: urlPath.path, type });
+        }
+        console.log(`Saved screenshot of ${urlPath.url} to ${urlPath.path}`);
+      });
+    }, { concurrency: 6 });
+  })
 };
 
 export default urlToImage;
